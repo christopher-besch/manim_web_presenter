2cb20034b69952708bedcd74cd37ef16e300fd93
Christopher Besch <christopher.besch@gmx.de>
2021-09-07 11:17:30 +0200

    wrapper finished but untested

--- manim_wrappers/presentation.py
+++ manim_wrappers/presentation.py
@@ -9,28 +9,28 @@ GLOBAL_OUTPUT_FOLDER = "presentation"
 
 # represent
 class Slide:
-    def __init__(self, slide_type: str, number: int, name: str, first_animation: int):
-        self.slide_type = slide_type,
-        self.number = number
+    def __init__(self, slide_type: str, name: str, first_animation: int):
+        self.slide_type = slide_type
         # names are not intended to be unique
         self.name = name
-        self.first_animation: int = first_animation
-        self.last_animation: int = first_animation
+        # inclusive
+        self.first_animation = first_animation
+        # exclusive
+        self.after_last_animation = first_animation
 
     def empty(self) -> bool:
-        return self.first_animation == self.last_animation
-
-    def add_animation(self) -> None:
-        self.last_animation += 1
+        return self.first_animation == self.after_last_animation
 
     def get_dict(self) -> Dict:
         return {
             "slide_type": self.slide_type,
-            "number": self.number,
             "name": self.name,
             "first_animation": self.first_animation,
-            "last_animation": self.last_animation,
+            "after_last_animation": self.after_last_animation,
         }
+
+    def __repr__(self):
+        return f"<Slide '{self.name}' from {self.first_animation} to {self.after_last_animation}>"
 
 
 # represent entire presentation replacing a single manim scene
@@ -39,8 +39,9 @@ class Presentation(manim.Scene):
         super().__init__(*args, **kwargs)
 
         self.slides: List[Slide] = []
-        self.last_animation = 0
-        self.start_animation = -1
+        # paths to intermediate files created by manim
+        self.files: List[str] = []
+        self.next_animation = 0
 
         # create or clear global output folder
         if os.path.exists(GLOBAL_OUTPUT_FOLDER):
@@ -53,33 +54,30 @@ class Presentation(manim.Scene):
         # contain everything required to play this presentation including video files
         if not os.path.exists(self.output_folder):
             os.mkdir(self.output_folder)
-        # stores information
+        # stores intel about how to present slides
         self.intel_file = os.path.join(self.output_folder, f"{slide_name}.json")
 
-        # first slide can't have a loop
+        # first slide can be replaced with a loop
         self.next_normal_slide()
 
-    def __del__(self):
-        self.__finish_last_slide()
-        assert len(self.slides) != 0, "The presentation doesn't contain any animations."
+    def play(self, *args, **kwargs):
+        super().play(*args, **kwargs)
+        # exclusive -> store index of not yet defined animation
+        self.next_animation += 1
+        self.slides[-1].after_last_animation = self.next_animation
 
     def __finish_last_slide(self):
         # empty slides are confusing and will be overwritten
-        if len(self.slides) != 0 and self.slides[-1].empty:
+        if len(self.slides) != 0 and self.slides[-1].empty():
             self.slides.pop()
 
     def __next_slide(self, slide_type: str, name: Optional[str]):
         if name is None:
-            name = f"Unnamed Slide ({slide_type}) #{len(self.slides)}"
+            name = f"Slide ({slide_type}) #{len(self.slides)}"
         self.__finish_last_slide()
         self.slides.append(Slide(slide_type,
-                                 len(self.slides),
                                  name,
-                                 self.last_animation + 1))
-
-    def play(self, *args, **kwargs):
-        super().play(*args, **kwargs)
-        self.last_animation += 1
+                                 self.next_animation))
 
     # end last slide and start new (first slide has been created automatically)
     def next_normal_slide(self, name: Optional[str] = None):
@@ -101,16 +99,21 @@ class Presentation(manim.Scene):
         manim.config.max_files_cached = max_files_cached
 
         # copy intermediate video files
-        files = []
+        # update files list
+        self.files = []
         for src_file in self.renderer.file_writer.partial_movie_files:
-            print(src_file)
             assert src_file.endswith(".mp4"), "Only mp4 files are supported; Did you add a wait or play statement to the presentation?"
             dst_file = os.path.join(self.output_folder, os.path.basename(src_file))
             shutil.copyfile(src_file, dst_file)
-            files.append(dst_file)
+            self.files.append(dst_file)
+
+    def tear_down(self, *args, **kwargs):
+        self.__finish_last_slide()
+        assert len(self.slides) != 0, "The presentation doesn't contain any animations."
 
         with open(self.intel_file, "w") as file:
             json.dump({
-                "slides": [slide.get_dict for slide in self.slides],
-                "files": files
+                "slides": [slide.get_dict() for slide in self.slides],
+                "files": self.files
             }, file)
+        super().tear_down(*args, **kwargs)
