ae399d158658f9f72bc2a545f66d43a6bcb9629c
MarcasRealAccount <thetremine.lumicks@gmail.com>
2021-09-08 19:05:42 +0200

    Video player styling and started work on the manim controller

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,3 +1,5 @@
+let manim_web_controls: Window | null;
+
 // download file and parse json
 function get_json(url: string, callback: { (response: any, success: boolean): void; }): void {
     let request = new XMLHttpRequest();
@@ -17,8 +19,20 @@ function get_json(url: string, callback: { (response: any, success: boolean): vo
     request.send();
 }
 
+// Open manim web controls window
+function open_manim_web_controls(): boolean {
+    // If the manim web controls window is not opened, then open a popup window
+    if (manim_web_controls == null || manim_web_controls.closed) {
+        manim_web_controls = window.open("/fallback.html", "Manim Web Controls", "resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no,status=no");
+        return manim_web_controls != null;
+    } else {
+        manim_web_controls.focus();
+        return true;
+    }
+}
+
 document.body.onload = () => {
     get_json("index.json", (response, success) => {
         console.log(response);
-    })
+    });
 }
