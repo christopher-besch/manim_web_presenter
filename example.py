a87b9da7c3092146e6a734c744ff43ac7a330110
Christopher Besch <christopher.besch@gmx.de>
2021-09-20 12:48:10 +0200

    added satisfying animation

--- example.py
+++ example.py
@@ -1,24 +1,21 @@
 from manim_web_presenter import *
 
 
-class PointWithTrace(Presentation):
+class MovingGroupToTarget(Presentation):
     def construct(self):
-        path = VMobject()
-        dot = Dot()
-        path.set_points_as_corners([dot.get_center(), dot.get_center()])
-
-        def update_path(path: VMobject):
-            previous_path = path.copy()
-            previous_path.add_points_as_corners([dot.get_center()])
-            path.become(previous_path)
-
-        path.add_updater(update_path)
-        self.add(path, dot)
+        group = VGroup(
+            Dot(LEFT),
+            Dot(ORIGIN),
+            Dot(RIGHT, color=RED),
+            Dot(2*RIGHT).scale(1.4)
+        )
+        target = Dot([4, 3, 0], color=YELLOW)
+        self.add(group, target)
         self.next_skip_slide()
-        self.play(Rotating(dot, radians=2*PI, about_point=RIGHT, run_time=2))
+        self.play(group.animate.shift(target.get_center() - group[2].get_center()))
         self.next_complete_loop_slide()
-        self.play(Rotating(dot, radians=2*PI, about_point=RIGHT, run_time=2))
+        self.play(group.animate.shift(LEFT))
+        self.play(group.animate.shift(2*RIGHT))
+        self.play(group.animate.shift(LEFT))
         self.next_normal_slide()
-        self.play(dot.animate.shift(UP))
-        self.play(dot.animate.shift(LEFT))
-        self.wait()
+        self.play(FadeOut(group, target))
